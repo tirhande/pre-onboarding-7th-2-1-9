78e75bb26e5b2917eeb7243dd9e26fcfb0aacd4c
Kyunghun Kim <74575497+tirhande@users.noreply.github.com>
2022-11-02 21:04:37 +0900

    fix : 버그 수정

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,6 @@ import MainContainer from '../src/container/Main/Main';
 import { CarService } from '../src/api/request';
 import { useCar, useCarChange } from '../src/stores/CarListProvider';
 import { CarActionType } from '../src/stores/ActionTypes';
-import { NextSeo } from 'next-seo';
 
 const Main = ({ payload }) => {
   const dispatch = useCarChange();
@@ -17,19 +16,6 @@ const Main = ({ payload }) => {
 
   return (
     <>
-      <NextSeo
-        title="피플카!"
-        description="짧은 여정부터 긴 여정까지 당신의 모든 여정, 피플카"
-        canonical="https://www.peoplecar.co.kr"
-        openGraph={{
-          type: 'website',
-          url: 'https://www.peoplecar.co.kr',
-          title: '피플카!',
-          description: '짧은 여정부터 긴 여정까지 당신의 모든 여정, 피플카',
-          images: [{ url: 'https://peoplecar.co.kr/src/images/common/og.png' }],
-          site_name: 'peoplecar',
-        }}
-      />
       <Layout content={<MainContainer />} />
     </>
   );
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -6,7 +6,9 @@ import { MainLayout } from './styles';
 const Layout = ({ content, isDetail }) => {
   return (
     <>
-      <Head><title>B2C 차량대여 서비스</title></Head>
+      <Head>
+        <title>B2C 차량대여 서비스</title>
+      </Head>
       <Header isDetail={isDetail} />
       <MainLayout>{content}</MainLayout>
     </>
